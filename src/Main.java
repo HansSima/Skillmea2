b1ae4097fe42c476cb11d9a7db1f2bc624a59ff3
HansSima <SimaHonza@seznam.cz>
2025-05-27 19:21:50 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        String pozdrav = "Ahoj svete";
+        /*String pozdrav = "Ahoj svete";
 
         //Toto je pro vypsani textu
         System.out.println(pozdrav);
         System.out.println(pozdrav);
         System.out.println(pozdrav);
-        System.out.println(pozdrav);
+        System.out.println(pozdrav);*/
+
+        int prvniCislo = 10;
+        int druheCislo = 5;
+
+        System.out.println(prvniCislo + druheCislo);
+        System.out.println(prvniCislo - druheCislo);
+        System.out.println(prvniCislo * druheCislo);
+        System.out.println(prvniCislo / druheCislo);
 
 
     }
