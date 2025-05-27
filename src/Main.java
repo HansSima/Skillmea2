f97930ec795e2b2297b53c89d1daed8ba9b5fc1a
HansSima <SimaHonza@seznam.cz>
2025-05-27 19:04:56 +0200

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        String pozdrav = "Ahoj svete";
 
-
-        System.out.println("Ahoj svete");
-
-
+        //Toto je pro vypsani textu
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
+        System.out.println(pozdrav);
 
 
     }
